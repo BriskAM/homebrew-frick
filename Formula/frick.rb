c7a531d9852cd453de74516fb3bfb84bec95351b
Akshit Mehta <akshitmehta0136@gmail.com>
2026-07-13 15:48:51 +0530

    Update frick formula to v0.0.9 with local offline cache support

--- Formula/frick.rb
+++ Formula/frick.rb
@@ -1,23 +1,23 @@
 class Frick < Formula
   desc "Lightning-fast command-line corrector powered by Gemini/Groq APIs"
   homepage "https://github.com/BriskAM/frick"
-  version "0.0.5"
+  version "0.0.9"
 
   if OS.mac?
     if Hardware::CPU.intel?
       url "https://github.com/BriskAM/frick/releases/download/v#{version}/frick-darwin-amd64"
-      sha256 "685d74ea8a04adbac1e229e9a3e50916d825843d0087b0ef38e281a3cd366435"
+      sha256 "8427f992b464fd1b6c1345d10010da98007a27627b24de11894167501e3b7993"
     elsif Hardware::CPU.arm?
       url "https://github.com/BriskAM/frick/releases/download/v#{version}/frick-darwin-arm64"
-      sha256 "3416a66c68ab787c6264710f9ffb8eb15c04e070e5b818cf4cd6861cca4970ae"
+      sha256 "d8e3363bf2efde017de30d090ffd774ae2ea99b5d5e25ce98feb054cb7359c4e"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/BriskAM/frick/releases/download/v#{version}/frick-linux-amd64"
-      sha256 "39c3bd2485a313b662711cfa30f9eb620eb515ad9164f80be7255dd9094618f8"
+      sha256 "2439d7de2419cf404712e371ef189e72533b39c972ae2dd78bf7f0634587277f"
     elsif Hardware::CPU.arm?
       url "https://github.com/BriskAM/frick/releases/download/v#{version}/frick-linux-arm64"
-      sha256 "276ddb7ab11fb68e407731ac2a545a95e44740ced5adff26ac4bf30388bb1be8"
+      sha256 "920bba3ece5c64d6646207cce108d9eff15889500ce9b2cafdc02baca7b2fb6f"
     end
   end
 
