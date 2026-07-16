e188702e5f1be993f455caabc208efba21c8336b
Akshit Mehta <akshitmehta0136@gmail.com>
2026-07-16 17:37:58 +0530

    Update frick formula to v0.1.0 with POSIX standard fc support for Bash

--- Formula/frick.rb
+++ Formula/frick.rb
@@ -1,23 +1,23 @@
 class Frick < Formula
   desc "Lightning-fast command-line corrector powered by Gemini/Groq APIs"
   homepage "https://github.com/BriskAM/frick"
-  version "0.0.9"
+  version "0.1.0"
 
   if OS.mac?
     if Hardware::CPU.intel?
       url "https://github.com/BriskAM/frick/releases/download/v#{version}/frick-darwin-amd64"
-      sha256 "8427f992b464fd1b6c1345d10010da98007a27627b24de11894167501e3b7993"
+      sha256 "67f6132406776b4caa2299b6cbf004919d88c7bac8a4f3b4432ca8e76f508cb1"
     elsif Hardware::CPU.arm?
       url "https://github.com/BriskAM/frick/releases/download/v#{version}/frick-darwin-arm64"
-      sha256 "d8e3363bf2efde017de30d090ffd774ae2ea99b5d5e25ce98feb054cb7359c4e"
+      sha256 "6f7d8bb5fe590892a6a9cc5fc3d24b6206c359c259bca6cc5b0455f7bb32d900"
     end
   elsif OS.linux?
     if Hardware::CPU.intel?
       url "https://github.com/BriskAM/frick/releases/download/v#{version}/frick-linux-amd64"
-      sha256 "2439d7de2419cf404712e371ef189e72533b39c972ae2dd78bf7f0634587277f"
+      sha256 "77c90d03078b172fd1ad0b023a4acb9ba49bff2caea82b1f15cf9fad7730e261"
     elsif Hardware::CPU.arm?
       url "https://github.com/BriskAM/frick/releases/download/v#{version}/frick-linux-arm64"
-      sha256 "920bba3ece5c64d6646207cce108d9eff15889500ce9b2cafdc02baca7b2fb6f"
+      sha256 "66a4781ef63171936e0bf23c4ee8124fbc2e5cfff5cf735e300c2a9458d1d75b"
     end
   end
 
